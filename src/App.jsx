0c7f452cdef636dc9b30d7c9f95c79ee7fbc8ff9
woominko <woomin422@gmail.com>
2025-09-29 03:25:30 +0900

    init

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import "./App.css"
+
+
+function App() {
+  
+
+  return (
+    <>
+    <table border="1">
+    <tr>
+      <th></th>
+      <th></th>
+      <th></th>
+    </tr>
+    <tr>
+      <td></td>
+      <td></td>
+      <td></td>
+    </tr>
+    <tr>
+      <td></td>
+      <td></td>
+      <td></td>
+    </tr>
+</table>
+    </>
+  )
+}
+
+export default App
